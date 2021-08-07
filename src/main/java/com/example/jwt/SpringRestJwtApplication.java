b1792331c397eb904cc11ba9e5f49d3b62a791cb
Pedro Lacombe <pedrocarlantonio@gmail.com>
2021-08-07 10:26:21 -0300

    Project Structure

---
+++ src/main/java/com/example/jwt/SpringRestJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestJwtApplication.class, args);
+	}
+
+}
